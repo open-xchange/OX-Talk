2159a94d87cf100d33474c6cb8de6672442c2ddf
Frank Gregor <frank.gregor@open-xchange.com>
2019-09-09 13:40:11 +0200

    introduces SwiftLint to the project

--- ios/OX Coi/Application/AppDelegate.swift
+++ ios/OX Coi/Application/AppDelegate.swift
@@ -1,9 +1,10 @@
-import UIKit
 import Flutter
+import UIKit
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-    
+@objc
+class AppDelegate: FlutterAppDelegate {
+
     override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GeneratedPluginRegistrant.register(with: self)
 
