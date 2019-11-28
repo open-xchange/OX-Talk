40e501a3c9c97476f72a0aa66971844d601eb4af
Cihan Oezkan <cihan.oezkan@open-xchange.com>
2019-11-28 12:44:20 +0100

    OT-553 #311 Overlapping buttons in Profile

--- lib/src/user/user_profile.dart
+++ lib/src/user/user_profile.dart
@@ -193,6 +193,7 @@ class _ProfileState extends State<UserProfile> {
                 ),
               ],
             ),
+            Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 0)),
             AdaptiveRaisedButton(
               child: Text(L10n.get(L.profileShareInviteUrl)),
               onPressed: createInviteUrl,
