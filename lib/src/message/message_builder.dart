ruo2012/ox-coi

Forward state for messages #337

lib/src/message/message_builder.dart
@@ -48,6 +48,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ox_coi/src/adaptiveWidgets/adaptive_icon.dart';
+import 'package:ox_coi/src/l10n/l.dart';
+import 'package:ox_coi/src/l10n/l10n.dart';
 import 'package:ox_coi/src/message/message_attachment_bloc.dart';
 import 'package:ox_coi/src/message/message_attachment_event_state.dart';
 import 'package:ox_coi/src/message/message_item_bloc.dart';
@@ -118,6 +120,33 @@ class MessageText extends StatelessWidget {
     );
   }
 }
+
+class MessagePartForwarded extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var messageStateData = _getMessageStateData(context);
+    Color color = messageStateData.isOutgoing ? onSecondary.withOpacity(half) : onSurface.withOpacity(half);
+    double topPadding = messageStateData.isGroup && !messageStateData.isOutgoing ? messagesForwardTopPaddingGroup : messagesForwardTopPadding;
+    return Padding(
+      padding: EdgeInsets.only(top: topPadding, left: messagesHorizontalInnerPadding, right: messagesHorizontalInnerPadding,),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          AdaptiveIcon(
+            icon: IconSource.forward,
+            color: color,
+            size: messagesForwardIconSize,
+          ),
+          Text(
+            L10n.get(L.forwarded),
+            style: Theme.of(context).textTheme.caption.apply(color: color),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 
 String _getText(BuildContext context) {
   return MessageData.of(context).useInformationText ? _getMessageStateData(context).informationText : _getMessageStateData(context).text;
@@ -500,7 +529,7 @@ class MessagePartFlag extends StatelessWidget {
 
 EdgeInsetsGeometry getNamePaddingForGroups(BuildContext context) {
   var messageStateData = _getMessageStateData(context);
-  if (messageStateData.isGroup && !messageStateData.isOutgoing) {
+  if (messageStateData.isGroup && !messageStateData.isOutgoing || messageStateData.isForwarded) {
     return EdgeInsets.only(
       top: 2.0,
       bottom: messagesVerticalInnerPadding,

open-xchange/OX-Talk

lib/src/message/message_builder.dart
@@ -53,6 +53,8 @@ import 'package:ox_coi/src/brandable/custom_theme.dart';
 import 'package:ox_coi/src/extensions/color_apis.dart';
 import 'package:ox_coi/src/extensions/numbers_apis.dart';
 import 'package:ox_coi/src/extensions/string_markdown.dart';
+import 'package:ox_coi/src/l10n/l.dart';
+import 'package:ox_coi/src/l10n/l10n.dart';
 import 'package:ox_coi/src/message/message_attachment_bloc.dart';
 import 'package:ox_coi/src/message/message_attachment_event_state.dart';
 import 'package:ox_coi/src/message/message_item_bloc.dart';
@@ -130,6 +132,33 @@ Future<void> _launch({@required String url}) async {
     await launch(url);
   }
 }
+
+class MessagePartForwarded extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var messageStateData = _getMessageStateData(context);
+    Color color = messageStateData.isOutgoing ? CustomTheme.of(context).onSecondary.half() : CustomTheme.of(context).onSurface.half();
+    double topPadding = messageStateData.isGroup && !messageStateData.isOutgoing ? dimension2dp : dimension8dp;
+    return Padding(
+      padding: EdgeInsets.only(top: topPadding, left: messagesHorizontalInnerPadding, right: messagesHorizontalInnerPadding,),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          AdaptiveIcon(
+            icon: IconSource.forward,
+            color: color,
+            size: dimension16dp,
+          ),
+          Text(
+            L10n.get(L.forwarded),
+            style: Theme.of(context).textTheme.caption.apply(color: color),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 
 String _getText(BuildContext context) {
   return MessageData.of(context).useInformationText ? _getMessageStateData(context).informationText : _getMessageStateData(context).text;
@@ -519,7 +548,7 @@ class MessagePartFlag extends StatelessWidget {
 
 EdgeInsetsGeometry getNamePaddingForGroups(BuildContext context) {
   var messageStateData = _getMessageStateData(context);
-  if (messageStateData.isGroup && !messageStateData.isOutgoing) {
+  if (messageStateData.isGroup && !messageStateData.isOutgoing || messageStateData.isForwarded) {
     return EdgeInsets.only(
       top: dimension2dp,
       bottom: messagesVerticalInnerPadding,