bartektomas/ox-coi

Moved key generation into the dart part #501 OT-779 Key handling includes generating, persisting, getting, converting Refactored the method channel handling on Android Refactored the method channel handling on Dart / Flutter side Cleaned up Android native part Renaming and wrapping to better understand sharing flows

lib/src/share/share_bloc.dart
@@ -44,21 +44,19 @@ import 'dart:io';
 
 import 'package:bloc/bloc.dart';
 import 'package:delta_chat_core/delta_chat_core.dart';
-import 'package:flutter/services.dart';
 import 'package:ox_coi/src/chatlist/chat_list_bloc.dart';
 import 'package:ox_coi/src/chatlist/chat_list_event_state.dart';
 import 'package:ox_coi/src/contact/contact_list_bloc.dart';
 import 'package:ox_coi/src/contact/contact_list_event_state.dart';
 import 'package:ox_coi/src/data/contact_repository.dart';
+import 'package:ox_coi/src/platform/method_channels.dart';
 import 'package:ox_coi/src/platform/preferences.dart';
 import 'package:ox_coi/src/share/share_event_state.dart';
-import 'package:ox_coi/src/share/shared_data.dart';
-import 'package:ox_coi/src/utils/constants.dart';
+import 'package:ox_coi/src/share/incoming_shared_data.dart';
 
 class ShareBloc extends Bloc<ShareEvent, ShareState> {
   ChatListBloc _chatListBloc = ChatListBloc();
   ContactListBloc _contactListBloc = ContactListBloc();
-  static const sharingChannel = const MethodChannel(kMethodChannelSharing);
 
   @override
   ShareState get initialState => ShareStateInitial();
@@ -130,10 +128,10 @@ class ShareBloc extends Bloc<ShareEvent, ShareState> {
       return;
     }
     if (data.length > 0) {
-      var sharedData = SharedData(data);
+      var sharedData = IncomingSharedData(data);
       add(SharedDataLoaded(sharedData: sharedData));
     }
   }
 
-  Future<Map> _getSharedData() async => await sharingChannel.invokeMethod('getSharedData');
+  Future<Map> _getSharedData() async => await SharingChannel.instance.invokeMethod(SharingChannel.kMethodGetSharedData);
 }

open-xchange/OX-Talk

lib/src/share/share_bloc.dart
@@ -42,20 +42,18 @@
 
 import 'package:bloc/bloc.dart';
 import 'package:delta_chat_core/delta_chat_core.dart';
-import 'package:flutter/services.dart';
 import 'package:ox_coi/src/chatlist/chat_list_bloc.dart';
 import 'package:ox_coi/src/chatlist/chat_list_event_state.dart';
 import 'package:ox_coi/src/contact/contact_list_bloc.dart';
 import 'package:ox_coi/src/contact/contact_list_event_state.dart';
 import 'package:ox_coi/src/data/contact_repository.dart';
+import 'package:ox_coi/src/platform/preferences.dart';
 import 'package:ox_coi/src/share/share_event_state.dart';
-import 'package:ox_coi/src/share/shared_data.dart';
-import 'package:ox_coi/src/utils/constants.dart';
+import 'package:ox_coi/src/share/incoming_shared_data.dart';
 
 class ShareBloc extends Bloc<ShareEvent, ShareState> {
   ChatListBloc _chatListBloc = ChatListBloc();
   ContactListBloc _contactListBloc = ContactListBloc();
-  static const sharingChannel = const MethodChannel(kMethodChannelSharing);
 
   @override
   ShareState get initialState => ShareStateInitial();
@@ -134,10 +132,10 @@ class ShareBloc extends Bloc<ShareEvent, ShareState> {
       return;
     }
     if (data.length > 0) {
-      var sharedData = SharedData(data);
+      var sharedData = IncomingSharedData(data);
       add(SharedDataLoaded(sharedData: sharedData));
     }
   }
 
-  Future<Map> _getSharedData() async => await sharingChannel.invokeMethod('getSharedData');
+  Future<Map> _getSharedData() async => await SharingChannel.instance.invokeMethod(SharingChannel.kMethodGetSharedData);
 }